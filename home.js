d3851e9a2e1ea771e839ca93aec0d01f3caa524b
BARATH KUMAR <70008103+barathkumar10@users.noreply.github.com>
2021-09-08 16:07:18 +0530

    Add files via upload
    TODAY works of JAVASCRPIT

---
+++ home.js
@@ -0,0 +1,59 @@
+//console.log('hello');
+//alert('hello ths is barath');
+//var b='smoothie';
+//console.log(b);
+
+//var someNumber=45;
+//console.log(someNumber);
+ 
+
+//var age=prompt('what ur age');
+//document.getElementById('someText').innerHTML=age;
+
+//numbers in javascript
+
+/*var num=4.7;
+console.log(5*10);
+console.log(50/10);*/
+
+/*var num1=10;
+//num=num1+1;
+//console.log(num);
+
+//increment num1
+num1++;
+
+//decrement num1;
+num1--;
+console.log(num1);
+
+num1+=5;
+console.log(num1);*/
+
+///////////////FUNCTIONS//////////
+/*function fun(){
+    alert('this is a barath');
+}
+
+fun();*/
+
+/*function greeting(){
+    var name=prompt('what is your name');
+    var result='hello!'+ ' '+name;
+    console.log(result);
+}
+greeting();*/
+
+/*function sumNumber(num1,num2){
+    var result=num1+num2;
+    console.log(result);
+}
+//sumNumber('sarath','barath');
+sumNumber(10,10);*/
+
+var num=1;
+
+while (num<100){
+    num+=1;
+    console.log(num);
+}
