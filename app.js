2f18a34b8c072d45876932411ab937d1389afe5e
BARATH KUMAR <70008103+barathkumar10@users.noreply.github.com>
2021-09-19 13:01:06 +0530

    19/09/2021

--- app.js
+++ app.js
@@ -1,15 +1,77 @@
-//alert('hello');
-//console.log('hello');
-function ageInDays(){
-var BirthYear= prompt('what year were you born..good friend');
-var ageInDayss = (2018-birthYear)*365;
-var h1=document.createElement('h1');
-var textAnswer=document.createTextNode('you are '+ageInDayss+'days old.');
-h1.setAttribute('id','ageInDays');
-h1.appendChild(textAnswer);
-document.getElementById('flex-box-result').appendChild(h1);
+/*function teamname(){
+    console.log("MI");
+    console.log("CSK");
+    console.log("KKR");
+    console.log("RCB");
+}
+teamname()
+
+function greet(firstName){
+    console.log(`hello sir, ${firstName}!`);
 }
 
-function reset(){
-document.getElementById('ageInDays').remove();
+function greet(firstName, lastName){
+    console.log(`hello sir, ${firstName} ${lastName[0]}.`)
 }
+
+
+function repeat(str,numTimes){
+    let result='';
+    for(let i=0;i<numTimes;i++){
+        result+=str;
+    }
+    console.log(result);
+}
+
+
+function add(x,y){
+    if(typeof x!=='number'||typeof y!=='number'){
+        return false;
+    }
+    return x+y;
+}
+
+
+function iplteams(){
+    let totalteams=8;
+}
+
+
+let team='mumbai indian';
+function iplTeam(){
+    let ipl='king of ipl';
+    console.log(ipl);
+}
+
+let team='mumbai indian';
+function iplTeam(){
+    let ipl='king of ipl';
+    console.log(ipl);
+}
+
+iplTeam()
+
+
+
+let radius =8;
+if (radius>0){
+    const PI=3.14159;
+    let msg='HIII!'
+}
+
+console.log(radius);
+
+
+for (let i=0;i<5;i++){
+    let msg="qwwefqgargdrg";
+    console.log(msg)
+}
+console.log(msg)
+
+
+for (var i=0;i<5;i++){
+    var msg="qwwefqgargdrg";
+    console.log(msg)
+}
+console.log(msg)
+*/
